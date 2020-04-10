18320f457849788c07ef1dedb2209b1cc7d2ddb2
Roman.Rojas <roman.rojas@suddath.com>
2020-04-10 07:46:19 -0400

    Incremental update

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import 'react-native-gesture-handler'
-import React from "react"
-import { Provider } from "react-redux"
-import { store, rrfProps } from "./src/store"
-import { ReactReduxFirebaseProvider } from "react-redux-firebase"
-import MyApp from "./src/index"
-import { NavigationContainer } from '@react-navigation/native'
+import "react-native-gesture-handler";
+import React from "react";
+import { Provider } from "react-redux";
+import { store, rrfProps } from "./src/store";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import MyApp from "./src/index";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Provider store={ store }>
-                <ReactReduxFirebaseProvider { ...rrfProps }>
-                    <MyApp/>
-                </ReactReduxFirebaseProvider>
-            </Provider>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <MyApp />
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    </NavigationContainer>
+  );
 }
